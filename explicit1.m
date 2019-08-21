e7c89dbd3cc12ca39f77920d4846f73af94e3617
Kenn <kennethassogba@gmail.com>
2019-08-21 16:31:02 +0200

    Almost, continue harder

--- explicit1.m
+++ explicit1.m
@@ -7,6 +7,7 @@ dt	=t(2)-t(1);
 
 H0	=[1 0;0 2];
 H1	=[0 1;1 0];
+Id2 = eye (size (H1));
 
 alpha = 10;
 y0 = [1;0];
@@ -33,8 +34,8 @@ for iter=1:maxiter
   p = calculP(L,ycible,y,H0,H1,ctil,dt); %good place to be? No
   psib = calculPsib(y,L,H0,dt);
   for j=1:L-1
-    delta(j)=alpha/(alpha+dt*real(chit(:,j)'*(H1^2)*psib(:,j)));
-    c(j)=(1-delta(j))*ctil(j)-(delta(j)/alpha)*imag(chit(:,j)'*H1*psib(:,j));
+    delta(j)=alpha/(alpha+dt*real(chit(:,j).'*(H1^2)*psib(:,j)));
+    c(j)=(1-delta(j))*ctil(j)-(delta(j)/alpha)*imag(chit(:,j).'*H1*psib(:,j));
   end
   chit = calculChit(p,L,H0,dt);
   
@@ -42,8 +43,8 @@ for iter=1:maxiter
   psit=calculPsit(y,L,H0,dt);
   chib=calculChib(p,L,H0,dt);
   for j=1:L-1
-    etha(j)=alpha/(alpha+dt*real(chib(:,j)'*(H1^2)*psit(:,j)));
-    ctil(j)=(1-etha(j))*c(j)-(etha(j)/alpha)*imag(chib(:,j+1)'*H1*psit(:,j+1));
+    etha(j)=alpha/(alpha+dt*real(chib(:,j).'*(H1^2)*psit(:,j)));
+    ctil(j)=(1-etha(j))*c(j)-(etha(j)/alpha)*imag(chib(:,j+1).'*H1*psit(:,j+1));
   end
   
   J=fonctionelle(y0,L,H0,H1,dt,c,ycible,alpha);
@@ -54,19 +55,20 @@ for iter=1:maxiter
   legend ("Explicit scheme");
 	pause(.1);
   
-	fprintf(2,'Iter=%i|J=%f \n',iter,J)
+	fprintf(2,'Iter=%i, J=%f \n',iter,J)
 end
+fprintf(1,'y(T)=%f \n',y(:,L))
 
 end
 
 %============= Functions =================
-%quelle est la bonne fontionnelle ?
 %quel est son gradient?
 %ecrire le test debug dans une fonction
 function J=fonctionelle(y0,L,H0,H1,dt,c,ycible,alpha)
 
 y=calculY(y0,L,H0,H1,dt,c);
-J=2-norm(y(:,L)-ycible)^2 - alpha * dt * norm(c).^2;
+%J=2-norm(ycible-y(:,L))^2 - alpha * dt * norm(c)^2;
+J=2*real(ycible.'*y(:,L)) - alpha * dt * norm(c)^2;
 
 end
 %********** Psi (state) **********
@@ -74,15 +76,18 @@ function y=calculY(y0,L,H0,H1,dt,c)
 y=zeros(2,L);
 y(:,1)=y0;
 for j=2:L
-	y(:,j)=expm(-i*H0*dt)*expm(-i*H1*c(1,j-1)*dt)*y(:,j-1); %is it true?
+	y(:,j)=expm(-i*H0*dt/2)*expm(-i*H1*c(1,j-1)*dt)*expm(-i*H0*dt/2)*y(:,j-1); %is it true?
+  %y(:,j)=expm(i*H0*dt/2)*expm(i*H1*c(1,j-1)*dt)*expm(i*H0*dt/2)*y(:,j-1);
 end
 end
 %********** Chi (adjoint state) **********
 function p=calculP(L,ycible,y,H0,H1,ctil,dt)
 p=zeros(2,L);
-p(:,L)=y(:,L)-ycible; %to edit
+%p(:,L)=y(:,L)-ycible; %to edit
+p(:,L)=ycible; %to edit
 for j=L:-1:2
-	p(:,j-1)=expm(i*H1*ctil(1,j-1)*dt)*expm(i*H0*dt)*p(:,j); %is it true?
+	p(:,j-1)=expm(i*H0*dt/2)*expm(i*H1*ctil(1,j-1)*dt)*expm(i*H0*dt/2)*p(:,j); %is it true?
+  %p(:,j-1)=expm(-i*H0*dt/2)*expm(-i*H1*ctil(1,j-1)*dt)*expm(-i*H0*dt/2)*p(:,j);
 end	
 end
 %********** Psi Tilde **********
---
+++ implicit.m
@@ -0,0 +1,103 @@
+function implicit
+%============= Variables =================
+T	=10;
+L	=20; %return to 200
+t	=linspace(0,T,L);
+dt	=t(2)-t(1);
+
+H0	=[1 0;0 2];
+H1	=[0 1;1 0];
+
+
+alpha = 10;
+y0 = [1;0];
+ycible	=[0;1];
+
+maxiter	= 10; %return to 30 after
+c	= ones(1,L-1)*0.3; %eps
+ctil = ones(1,L-1)*0.5; %eps tilde
+%division by zero in mus if c=ctilde
+
+y=zeros(2,L);
+y(:,1)=y0;
+p=zeros(2,L);
+p(:,L)=ycible;%to edit?
+
+psit=zeros(2,L);
+psit(:,1)=expm(-(H0*dt)/(2*i))*y(:,1); %initial value
+
+psib=zeros(2,L);
+psib(:,1)=expm((H0*dt)/(2*i))*y(:,1);
+
+chit=zeros(2,L);
+chit(:,L)=expm((H0*dt)/(2*i))*p(:,L); %end value
+
+chib=zeros(2,L);
+chib(:,L)=expm(-(H0*dt)/(2*i))*p(:,L);
+
+Jtab=zeros(1,maxiter);
+
+
+%============= Main =================
+for iter=1:maxiter
+  %iter
+  %use Newton or secante?
+  %step1
+    for j=L-1:-1:1
+    chib(:,j+1)=expm(-(H0*dt)/(2*i))*p(:,j+1);
+    %psit(:,j)=expm(-(H0*dt)/(2*i))*y(:,j); %k+1
+    f = @(x)x+(1/alpha)*imag(chib(:,j+1).'*calculMus(c(j),x,H1,dt)*psit(:,j+1));
+    if (j==L-1)
+      x0=ctil(L-1);
+    else
+      x0=ctil(j+1);
+    endif
+    x=fzero(f,x0);
+    ctil(j)=x;
+    chit(:,j)=expm(i*H1*ctil(:,j)*dt)*chib(:,j+1);
+    
+    p(:,j)=expm(i*H0*dt/2)*chit(:,j);
+  end
+  %ctil
+  
+  
+  %step2
+  for j=1:L-1
+    psib(:,j)=expm((H0*dt)/(2*i))*y(:,j);
+    f = @(x)x+(1/alpha)*imag(chit(:,j).'*calculMus(x,ctil(j),H1,dt)*psib(:,j));
+    if (j==1)
+      x0=c(1);
+    else
+      x0=c(j-1);
+    endif
+    x=fzero(f,x0);
+    c(j)=x;
+
+    psit(:,j+1)=expm(-i*H1*c(:,j)*dt)*psib(:,j);
+    y(:,j+1)=expm(-i*H0*dt/2)*psit(:,j+1);
+  end
+  %c
+
+  
+  %J=2-norm(ycible-y(:,L))^2 - alpha * dt * norm(c)^2;
+  J=2*real(ycible.'*y(:,L)) - alpha * dt * norm(c)^2;
+  
+  Jtab(iter)=J;
+	plot(Jtab);
+  xlabel("Number of iteration");
+  ylabel ('{\it J_{\Delta T}(\epsilon)}')
+  legend ("Implicit scheme");
+	pause(.1);
+  
+	fprintf(2,'Iter=%i|J=%f \n',iter,J)
+end
+fprintf(1,'y(T)=%f \n',y(:,L))
+
+end
+
+%============= Functions =================
+%********** Mu star **********
+function mus=calculMus(x,y,mu,dt)
+  Id2 = eye (size (mu));
+  mus=i*(expm(mu*(x-y)*dt/i)-Id2)/(dt*(x-y));
+end
